dd4f3731e205355e060a1eb996110039fae3c665
Prakhar Dwivedi <prakhar1872@gmail.com>
2019-08-14 04:00:45 +0530

    Auto stash before merge of "master" and "origin/master"

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('',indexView),
-    path('login/',userloginView),
     path('userloggedin/',userLoggedInView),
     path('voterid/',voterIDView),
+    path('login/user/',userloginView),
+    path('login/gov/',govloginView),
+    path('register/',registerView),
 ]
