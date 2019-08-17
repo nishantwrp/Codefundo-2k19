02f6bc7c6605c4fa0b0b5ccbcda8907a8aeec821
Nishant Mittal <mittalnishant14@outlook.com>
2019-08-17 19:49:44 +0530

    Deleted Useless Files

--- api/views.py
+++
@@ -1,8 +0,0 @@
-import requests
-from django.http import JsonResponse
-import json
-from app import api
-def testView(request):
-    # r = requests.get(api.get_api_url("contracts/?workflowId=2"),headers=api.get_api_headers())
-    r = requests.get(api.get_api_url("applications/workflows/2/"),headers=api.get_api_headers())
-    return JsonResponse(r.json(),safe=False)
--- letsvote/settings.py
+++ letsvote/settings.py
@@ -40,8 +40,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'frontend',
-    'app',
-    'api'
+    'app'
 ]
 
 MIDDLEWARE = [
